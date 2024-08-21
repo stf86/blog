c2e61ea8ff017f4786b450fc61197a24b4eca7b7
stf86 <stefan.vukovic@live.com>
2024-08-21 11:48:25 +0200

    forms, user accounts, and static files

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render, get_object_or_404
+# from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.all()
-    context = {"posts": posts}
-    return render(request, "home.html", context)
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    context = {"post": post}
-    return render(request, "post_detail.html", context)
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
